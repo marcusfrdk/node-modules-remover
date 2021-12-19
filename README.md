2d2d3626d8f890fde44e48341979dfe64401e584
Marcus Fredriksson <marcus@marcusfredriksson.com>
2021-12-19 23:57:26 +0100

    Improved documentation and added check to verify if path is a directory (in loop and argument)

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Node Modules Remover
+
+CAUTION: This script runs the `rm -rf` command to delete the folder.
+
+## Usage
+
+Make the script executable if you get "permission denied"
+
+```bash
+$ chmod +x remove.sh
+```
+
+Then run the script
+
+```bash
+$ ./remove.sh
+```
+
+## Arguments
+
+By default, the script will run from the current working directory. By adding a path as the first argument, the script will use that instead.
+
+### Example
+
+```bash
+$ ./remove.sh /path/to/folder
+```
+
+## Alias
+
+To make this script more useful, add the script's path as an alias to your .bashrc (Linux) or .zprofile (Mac).
+
+```bash
+...
+alias ALIAS_NAME='/absolute/path/to/the/remove.sh'
+...
+```
--- remove.sh
+++ remove.sh
@@ -3,12 +3,19 @@ ROOT_PATH=$1
 # If path is not manually set, defaults to current working directory
 if [[ ! $ROOT_PATH ]]; then
     ROOT_PATH=$(pwd)
+else
+    # Check if path exists
+    if [[ ! -d $ROOT_PATH ]]; then
+        echo "Path is not valid"
+        exit 1
+    fi 
 fi
+
 
 for dir in $(find $ROOT_PATH -maxdepth 5 -type d)
 do
     # Removed any folder called "node_modules" and ignores any sub-folders
-    if [[ $(basename $dir) == "node_modules" && $(echo "$dir" | grep -o "node_modules" | wc -l) -lt 2 ]]; then
+    if [[ $(basename $dir) == "node_modules" && $(echo "$dir" | grep -o "node_modules" | wc -l) -lt 2 && -d $dir ]]; then
         echo "Deleting $dir..."
         rm -rf $dir
     fi
